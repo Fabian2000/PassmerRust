9ded20826c6d207599be3527423f119530104598
Fabian2000 <fs.21012000@gmail.com>
2024-04-29 15:33:01 +0200

    Added MessageBox in case of crash

--- passmer/passmer/src-tauri/src/main.rs
+++ passmer/passmer/src-tauri/src/main.rs
@@ -1,8 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rfd::MessageDialog;
+use std::process::exit;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let health_state = tauri::Builder::default().run(tauri::generate_context!());
+
+    // In case of error, show a message dialog and exit the application
+    match health_state {
+        Ok(_) => {}
+        Err(e) => {
+            MessageDialog::new()
+                .set_title("Error")
+                .set_description(format!("{}", e))
+                .set_level(rfd::MessageLevel::Error)
+                .show();
+            exit(1);
+        }
+    }
 }
--- passmer/passmer/src/main.jsx
+++ passmer/passmer/src/main.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Main() {
+  useEffect(() => {
+
+    const preventCtrl = (e) => {
+      // Exclude Ctrl + V and Ctrl + C
+      if (e.ctrlKey && e.key === 'v') {
+        return;
+      }
+      if (e.ctrlKey && e.key === 'c') {
+        return;
+      }
+      if (e.ctrlKey) {
+        e.preventDefault();
+      }
+    };
+
+    const preventFunctionKeys = (e) => {
+      if (e.key.startsWith('F') && !isNaN(e.key.slice(1)) && parseInt(e.key.slice(1)) >= 1 && parseInt(e.key.slice(1)) <= 12) {
+        e.preventDefault();
+      }
+    };
+
+    const preventContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    const preventImageDrag = (e) => {
+      if (e.target.nodeName === 'IMG') {
+        e.preventDefault();
+      }
+    }
+
+    let isReleaseMode = true;
+    if (process.env.NODE_ENV === 'development') {
+      isReleaseMode = false;
+      console.log('Development mode');
+    }
+
+    if (isReleaseMode) {
+      window.addEventListener('keydown', preventCtrl);
+      window.addEventListener('keydown', preventFunctionKeys);
+      window.addEventListener('contextmenu', preventContextMenu);
+      window.addEventListener('dragstart', preventImageDrag);
+    }
+    return () => { 
+      if (isReleaseMode) {
+        window.removeEventListener('keydown', preventCtrl);
+        window.removeEventListener('keydown', preventFunctionKeys);
+        window.removeEventListener('contextmenu', preventContextMenu);
+        window.removeEventListener('dragstart', preventImageDrag);
+      }
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          {/*<Route path="/about">
+            <About />
+          </Route>*/}
+          <Route path="/" element={<App/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
